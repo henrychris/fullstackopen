4471e60a04b80bd345e695c4f14b87ee3b924870
Henry Ihenacho <64085210+henrychris@users.noreply.github.com>
2024-07-24 23:43:02 +0100

    close destructuring example

--- part1/new_example/src/App.jsx
+++ part1/new_example/src/App.jsx
@@ -1,10 +1,14 @@
-const Hello = (props) => {
+// destructuring
+const Hello = ({ name, age }) => {
+    const bornYear = () => new Date().getFullYear() - age;
+
     return (
-        <div>
+        <>
             <p>
-                Hello {props.name}, you are {props.age} years old
+                Hello {name}, you are {age} years old.
             </p>
-        </div>
+            <p>So you were probably born in {bornYear()}.</p>
+        </>
     );
 };
 
